1b25fd6f043e4adf60be78669fd398e32fb91580
lukas13on <luka.pc.pc@gmail.com>
2022-03-31 15:02:31 -0300

    Depuradores
    Adicionada a configuração para depurar o site em tempo real com o Chrome e com Node.

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-var express = require('express');
-const chromeLauncher = require('chrome-launcher');
+import express from 'express';
+import path from 'path';
+
 var app = express();
-var fs = require('fs');
+var port = process.env.PORT || 3000;
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-fs.readFile('static/index.html', function (err, data) {
-    if (err) {
-        throw err;
-    }
+app.get('/', function (request, ressponse) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-    app.get('/', function (request, response) {
-        response.setHeader('Content-Type', 'text/html');
-        response.send(data);
-    });
-
-    app.listen(app.get('port'), function () {
-
-        console.log("Node app is running at localhost:" + app.get('port'));
-
-        // Inicia o Chrome
-        chromeLauncher.launch({
-            startingUrl: 'http://localhost:' + app.get('port'),
-        }).then(chrome => {
-            console.log(`Chrome debugging port running on ${chrome.port}`);
-        });
-
-    });
-
-
-
+app.listen(port, function () {
+    console.log('Visite o site em: http://localhost:' + port);
 });
