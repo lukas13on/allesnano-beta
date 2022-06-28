5d2d3664d111ac3d5002bdecd3e723c188cd2824
Lucas de Oliveira Neitzke <luka.pc.pc@gmail.com>
2022-06-28 18:14:16 -0300

    bkp

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,34 +1,36 @@
 $(document).ready(function () {
-    headerClass();
+
     scrollPsy();
+
+    $(window).on("load scroll", function () {
+        scrollPsy();
+        headerClass();
+    });
+
 });
 
 function headerClass() {
-    $(window).on('scroll', function () {
-        var top = $(window).scrollTop();
-        if (top === 0) {
-            $("body").addClass("scroll-top").removeClass("scroll-scrolled");
-        } else {
-            $("body").addClass("scroll-scrolled").removeClass("scroll-top");
-        }
-    });
+    var top = $(window).scrollTop();
+    if (top === 0) {
+        $("body").addClass("scroll-top").removeClass("scroll-scrolled");
+    } else {
+        $("body").addClass("scroll-scrolled").removeClass("scroll-top");
+    }
 }
 
 function scrollPsy() {
-    $(window).on('scroll', function () {
-        var currentTop = $(window).scrollTop();
-        var headerTop = $("header").outerHeight();
-        var elems = $('section[id]');
-        elems.each(function (index) {
-            var elemTop = $(this).offset().top - headerTop;
-            var elemBottom = elemTop + $(this).outerHeight();
-            var id = $(this).attr('id');
-            var navLink = $('.navbar-nav .nav-item a[href="#' + id + '"]');
-            if (currentTop >= elemTop && currentTop <= elemBottom) {
-                navLink.parent().addClass('active');
-            } else {
-                navLink.parent().removeClass('active');
-            }
-        });
+    var currentTop = $(window).scrollTop();
+    var headerTop = $("header").outerHeight();
+    var elems = $('section[id]');
+    elems.each(function (index) {
+        var elemTop = $(this).offset().top - headerTop;
+        var elemBottom = elemTop + $(this).outerHeight();
+        var id = $(this).attr('id');
+        var navLink = $('.navbar-nav .nav-item a[href="#' + id + '"]');
+        if (currentTop >= elemTop && currentTop <= elemBottom) {
+            navLink.parent().addClass('active');
+        } else {
+            navLink.parent().removeClass('active');
+        }
     });
 }
